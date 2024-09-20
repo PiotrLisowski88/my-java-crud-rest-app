361ac155ec2691c695af87b932d370cd821420c9
PiotrLisowski88 <biuro@inprom.pl>
2024-09-20 19:03:05 +0200

    Initial commit of CRUD REST application

--- src/main/java/com/codingtree/app/rest/RestApiApplication.java
+++ src/main/java/com/codingtree/app/rest/RestApiApplication.java
@@ -1,20 +1,11 @@
 package com.codingtree.app.rest;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class RestApiApplication {
-
     public static void main(String[] args) {
-
-        Dotenv dotenv = Dotenv.load();
-
-
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
         SpringApplication.run(RestApiApplication.class, args);
     }
 }
